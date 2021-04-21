39ef148fa98e2c62ff2099d7a4ce3e298249da69
carteroesterling <carteroesterling@wustl.edu>
2021-04-21 15:36:19 -0500

    Updates

---
+++ precache-manifest.224240e7162bb59b1b2932785fb466b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e78583b7a057c901158",
+    "url": "/09-todo-react-carteroesterling/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "7e78583b7a057c901158",
+    "url": "/09-todo-react-carteroesterling/static/js/main.7e78583b.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/09-todo-react-carteroesterling/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "6b456082499b222f784a",
+    "url": "/09-todo-react-carteroesterling/static/js/runtime~main.6b456082.js"
+  },
+  {
+    "revision": "55b12d1ee10c60eb0d291d0e0b6c7c9f",
+    "url": "/09-todo-react-carteroesterling/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/todo-react/precache-manifest.857ce18bebcba928b3be6d240402ba41.js"
+  "/09-todo-react-carteroesterling/precache-manifest.224240e7162bb59b1b2932785fb466b4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/todo-react/index.html", {
+workbox.routing.registerNavigationRoute("/09-todo-react-carteroesterling/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
